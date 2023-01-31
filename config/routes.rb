ded57593a513bd3148aa1c8bfca23372597a85d9
gagan goyal <goyalgagan82@gmail.com>
2023-01-31 18:57:11 +0530

    Add scroll bottom js

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ root 'chatroom#index'
 get 'login', to: 'sessions#new'
 post 'login', to: 'sessions#create'
 delete 'logout', to: 'sessions#destroy'
+post 'message', to: 'messages#create'
+
+mount ActionCable.server, at: '/cable'
 end
